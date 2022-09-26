c0487bdc6dc73313c6f0e308f8ecbbc40ebc1661
Attinder <105198941+Attinderkahlon@users.noreply.github.com>
2022-09-26 18:12:40 +1000

    feat: custom color pallete added

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -3,9 +3,8 @@ type Props = {
 
 export default function Homepage() {
     return (
-        <div className="flex">
-
-            <h1>HOMEPAGE</h1>
+        <div className="flex  bg-envited-secondary-100 h-screen w-screen">
+            <div><h1>Imagine if <span>Snapchat</span> had events</h1></div>
         </div>
     );
 }
