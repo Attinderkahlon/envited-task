e1181d5e291cfe66a36d24804e89b4ac368deddb
Attinder <105198941+Attinderkahlon@users.noreply.github.com>
2022-09-26 20:52:52 +1000

    feat: page routes defined

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import Homepage from "./pages/homepage";
+import Homepage from './pages/homepage'
+import { Routes, Route } from 'react-router-dom'
+import Create from './pages/create'
+import Event from './pages/event'
 
 function App() {
   return (
-    <div><Homepage/></div>
-  );
+    <Routes>
+      <Route path='/' element={<Homepage />} />
+      <Route path='/create' element={<Create />} />
+      <Route path='/event' element={<Event />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
